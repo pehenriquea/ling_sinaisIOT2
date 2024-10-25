30184268c7841b550cc7005eb26f6be8a6561f1c
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2024-10-25 16:43:07 -0300

    Create Header.js

---
+++ serverReact/Header.js
@@ -0,0 +1,12 @@
+
+function Header () {
+
+    return (
+       <div className="bg-purple-500 text-white p-12 text-center font-semibold text-6xl mb-5">
+            Alfabeto Sinais
+       </div>
+    );
+
+}
+
+export default Header
